4559d934b596f5a844b6dfc93af4fe61fbf123da
HunterLM <hunter109@gmail.com>
2022-05-14 00:12:48 -0500

    added functions for search history, updated css.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,25 @@
 // start of working code 
+// define submit from "search" button on page
 var submit = document.getElementById("submit");
 
-var createH2 = document.createElement("h2");
-var createH3 = document.createElement("h3");
-var createH6 = document.createElement("h6");
+// define elements we need to create
 var createButton = document.createElement("button");
 
-function fetchWeather(event) {
-    event.preventDefault();
+// global for userCity
+var userCity;
 
-    var userCity = document.getElementById("cityInput").value;
+// function for handling search button event
+function submitHandler (event) {
+    event.preventDefault();
+    userCity = document.getElementById("cityInput").value;
+    fetchWeather();
+};
+
+// function to get weather data for user defined city
+function fetchWeather() {
+    // event.preventDefault();
+
+    
     var key = "88f56688097f82e5c0a415624cb95159";
     var units = "imperial";
     var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&appid=" + key + "&units=imperial";
@@ -20,15 +30,15 @@ function fetchWeather(event) {
             if (response.ok) {
                 return response.json()
             .then(function (data) {
-                console.log(data);
+                // console.log(data);
                 var city = data.city.name;
-                console.log(city);
+                // console.log(city);
                 var temp = data.list[0].main.temp;
-                console.log(temp);
+                // console.log(temp);
                 var wind = data.list[0].wind.speed;
-                console.log(wind);
+                // console.log(wind);
                 var humidity = data.list[0].main.humidity;
-                console.log(humidity);
+                // console.log(humidity);
                 // future forecast temps! can i use a loop to accomplish this? prob.
                 var temp1 = data.list[1].main.temp;
                 var temp2 = data.list[2].main.temp;
@@ -47,6 +57,7 @@ function fetchWeather(event) {
                 var humidity3 = data.list[3].main.humidity;
                 var humidity4 = data.list[4].main.humidity;
                 var humidity5 = data.list[5].main.humidity;
+                searchHistory(userCity);
                 printResults(city, temp, temp1, temp2, temp3, temp4, temp5, wind, wind1, wind2, wind3, wind4, wind5, humidity, humidity1, humidity2, humidity3, humidity4, humidity5);
             })
             };
@@ -55,6 +66,7 @@ function fetchWeather(event) {
        
 };
 
+// function for printing data to page
 function printResults(city, temp, temp1, temp2, temp3, temp4, temp5, wind, wind1, wind2, wind3, wind4, wind5, humidity, humidity1, humidity2, humidity3, humidity4, humidity5) {
 
     var searchedCity = document.getElementById("searchedCity");
@@ -62,10 +74,17 @@ function printResults(city, temp, temp1, temp2, temp3, temp4, temp5, wind, wind1
     var searchedWind = document.getElementById("searchedWind");
     var searchedHumidity = document.getElementById("searchedHumidity");
     var weatherFive = document.getElementById("weatherFive");
-    var createH3 = document.createElement("h3");
+    var forecastText = document.getElementById("forecastText");
+    var now = moment().format("MM/DD/YYYY");
+    var day1 = moment(now).add(1, "days").format("MM/DD/YYYY");
+    var day2 = moment(now).add(1, "days").format("MM/DD/YYYY");
+    var day3 = moment(now).add(1, "days").format("MM/DD/YYYY");
+    var day4 = moment(now).add(1, "days").format("MM/DD/YYYY");
+    var day5 = moment(now).add(1, "days").format("MM/DD/YYYY");
 
-    weatherFive.appendChild(createH3);
-    createH3.textContent = "5-Day Forecast";
+    forecastText.textContent = "5-Day Forecast: ";
+    forecastText.style.display = "block";
+    weatherFive.style.display = "block";
     searchedCity.textContent = city + "    (" + moment().format('L') + ")" ;
     searchedTemp.textContent = "Temp: " + temp + "\u2109";
     searchedWind.textContent = "Wind: " + wind + " MPH";
@@ -76,31 +95,63 @@ function printResults(city, temp, temp1, temp2, temp3, temp4, temp5, wind, wind1
     var day1Wind = document.getElementById("day1Wind");
     var day1Humidity = document.getElementById("day1Humidity");
 
-    // day1Date.textContent = moment().formmat('MMM/DD/YYYY').add(1, 'days')
-    day1Temp.textContent = temp1;
-    day1Wind.textContent = wind1;
-    day1Humidity.textContent = humidity1;
+    day1Date.textContent = day1;
+    day1Temp.textContent = "Temp: " + temp1 + "\u2109";
+    day1Wind.textContent = "Wind: " + wind1 + " MPH";
+    day1Humidity.textContent = "Humidity: " + humidity1 + "%";
 
-    day2Temp.textContent = temp2;
-    day2Wind.textContent = wind2;
-    day2Humidity.textContent = humidity2;
+    day2Date.textContent = day2;
+    day2Temp.textContent = "Temp: " + temp2 + "\u2109";
+    day2Wind.textContent = "Wind: " + wind2 + " MPH";
+    day2Humidity.textContent = "Humidity: " + humidity2 + "%";
 
-    day3Temp.textContent = temp3;
-    day3Wind.textContent = wind3;
-    day3Humidity.textContent = humidity3;
+    day3Date.textContent = day3;
+    day3Temp.textContent = "Temp: " + temp3 + "\u2109";
+    day3Wind.textContent = "Wind: " + wind3 + " MPH";
+    day3Humidity.textContent = "Humidity: " + humidity3 + "%";
 
-    day4Temp.textContent = temp4;
-    day4Wind.textContent = wind4;
-    day4Humidity.textContent = humidity4;
+    day4Date.textContent = day4;
+    day4Temp.textContent = "Temp: " + temp4 + "\u2109";
+    day4Wind.textContent = "Wind: " + wind4 + " MPH";
+    day4Humidity.textContent = "Humidity: " + humidity4 + "%";
 
-    day5Temp.textContent = temp5;
-    day5Wind.textContent = wind5;
-    day5Humidity.textContent = humidity5;
+    day5Date.textContent = day5;
+    day5Temp.textContent = "Temp: " + temp5 + "\u2109";
+    day5Wind.textContent = "Wind: " + wind5 + " MPH";
+    day5Humidity.textContent = "Humidity: " + humidity5 + "%";
 
+};
+
+// function for search history being clicked
+function historyHandler(event) {
+
+    var clickedCity = event.currentTarget.textContent;
+    userCity = clickedCity;
+    fetchWeather();
+}
+
+function searchHistory() {
+
+    // array of cities for history, no local storage so it only saves until refresh
+    const cities = [];
+    cities.push(userCity);
+    var append = document.getElementById("append");
+
+    // forEach city, make a button and append it
+    cities.forEach(function (city, i) {
+        var createButton = document.createElement("button");
+        createButton.setAttribute("type", "submit");
+        createButton.setAttribute("class", "btn btn-primary search newCity");
+        createButton.innerText = city;
+        append.append(createButton);
+        createButton.addEventListener("click", historyHandler);
+        
+    });
+    
 };
 
 // api.openweathermap.org/data/2.5/forecast?q=${userCity}&appid=${key}&units=imperial
 
 // event listener on submit button, fetch weather
-submit.addEventListener("click", fetchWeather);
+submit.addEventListener("click", submitHandler);
 
